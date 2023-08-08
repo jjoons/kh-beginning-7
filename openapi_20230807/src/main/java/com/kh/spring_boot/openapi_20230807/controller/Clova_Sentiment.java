bc14885121c334cb94103cd7a16ac4153335ea79
JJoonS <jjoons@jjoons.com>
2023-08-08 16:44:54 +0900

    0808 3

--- openapi_20230807/src/main/java/com/kh/spring_boot/openapi_20230807/controller/Clova_Sentiment.java
+++ openapi_20230807/src/main/java/com/kh/spring_boot/openapi_20230807/controller/Clova_Sentiment.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+// @Controller
 @RestController
 public class Clova_Sentiment {
   @Value("${NAVER_CLOUD_PLATFORM_CLIENT_ID}")
